ff8574b1a5b9e36dad740312482dce7639797d94
izzyara <izzybzb@gmail.com>
2024-10-07 01:07:11 -0400

    a

--- RandomWalk.java
+++ RandomWalk.java
@@ -1,17 +1,18 @@
 
 public class RandomWalk{
 
-
+public static int sumsteps = 0;
     public static void main (String[]args){
     
         int tests = 1;
         for (int x=1; x<=50; x++){
+            System.out.print("Test " + tests + ": ");
             Walk();
             tests ++;
-            System.out.println("Test " + tests + " : ");
             }
+        int average = sumsteps/50;
+        System.out.print("Average steps taken: " + average);
         }
-
 
     public static void Walk(){
         int position = 0;
@@ -19,8 +20,7 @@ public class RandomWalk{
         double forwardD = 3.5;
         double backwardD = -3.5;
         int steps = 0;
-
-        while(position<=forwardD || position>=backwardD){
+        while(position<=forwardD && position>=backwardD){
         num = (int)(Math.random()*2);
         if (num==0){
             position++;
@@ -30,7 +30,8 @@ public class RandomWalk{
             steps ++;
         }
     }
-    System.out.print("Steps taken: " + steps);
+    sumsteps += steps;
+    System.out.println("Steps taken: " + steps);
 
         }
     }
